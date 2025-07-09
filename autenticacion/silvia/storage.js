2650edc66c101cba40d656c1fea01aff70852106
ivan22663 <alejandrosantis93@gmail.com>
2025-07-09 16:42:03 -0500

    Delete autenticacion/silvia directory

--- autenticacion/silvia/storage.js
+++
@@ -1,8 +0,0 @@
-function getUsers() {
-    const users = localStorage.getItem('users');
-    return users ? JSON.parse(users) : [];
-}
-
-function generateId() {
-    return Math.random().toString(36).substr(2, 9);
-}
